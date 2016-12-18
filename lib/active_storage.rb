bfb9a2d9e1ddc667482409a8c9e9dc7c315334f2
Artem Biserov <artem.biserov@gmail.com>
2016-12-18 20:23:10 +0300

    implement count method

--- lib/active_storage.rb
+++ lib/active_storage.rb
@@ -1,15 +1,29 @@
 require "active_support/concern"
+require 'active_support/inflector'
+require "csv"
 
 module ActiveStorage
   extend ActiveSupport::Concern
 
+  private
+
   class_methods do
     def count
+      CSV.read(storage_path, col_sep: ";").size - 1 # There are headers
     end
 
     private
 
-    def path
+    def storage_file_name
+      "#{self.to_s.tableize}"
+    end
+
+    def storage_path
+      "#{database_path}/#{storage_file_name}.csv"
+    end
+
+    def database_path
+      "./database"
     end
   end
 end
--- spec/fixtures/product.rb
+++ spec/fixtures/product.rb
@@ -2,4 +2,10 @@ require "./lib/active_storage"
 
 class Product
   include ActiveStorage
+
+  private
+
+  def self.database_path
+    "./spec/fixtures"
+  end
 end
