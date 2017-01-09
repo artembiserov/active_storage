ff4e720239bb39298323d9d53494b4a9146589f2
Artem Biserov <artem.biserov@gmail.com>
2017-01-09 11:55:01 +0300

    remove unused instance_eval in association module

--- lib/active_storage/association.rb
+++ lib/active_storage/association.rb
@@ -1,26 +1,22 @@
 module ActiveStorage
   module Association
     def belongs_to(name, options = {})
-      instance_eval do
-        foreign_key = options[:foreign_key] || "#{name}_id"
+      foreign_key = options[:foreign_key] || "#{name}_id"
 
-        define_method name do
-          klass = name.to_s.classify.constantize
-          parent_id = public_send(foreign_key)
+      define_method name do
+        klass = name.to_s.classify.constantize
+        parent_id = public_send(foreign_key)
 
-          klass.where(id: parent_id).first
-        end
+        klass.where(id: parent_id).first
       end
     end
 
     def has_many(name, options = {})
-      instance_eval do
-        define_method name do
-          klass = name.to_s.classify.constantize
-          foreign_key = options[:foreign_key] || "#{self.class.name.underscore}_id"
+      define_method name do
+        klass = name.to_s.classify.constantize
+        foreign_key = options[:foreign_key] || "#{self.class.name.underscore}_id"
 
-          klass.where(foreign_key => id)
-        end
+        klass.where(foreign_key => id)
       end
     end
   end
